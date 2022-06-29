b6d83698b5083d5eb930cfbf732048db9e82b1bb
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-06-29 18:30:11 -0500

    .env

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+//connects to .env
+require('dotenv').config()
+
+//express application
 const express= require('express')
 const app = express()
 
+//route to /page
 app.get('/', (req, res) => {
     res.send('Jenni\s Book World')
 })
 
-app.listen(3003)
+app.listen(process.env.PORT)
